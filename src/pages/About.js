57c7d6f79e9d5c955173d6a008b1b14b8fa48780
resul <resulavsar003@gmail.com>
2022-04-03 12:06:38 +0200

    Router project

---
+++ src/pages/About.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "./About.module.css";
+
+
+const About = () => {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>
+        This page has been created by a curious full-blown developer with strong
+        creative thinking skills, high energy, and integrity, skilled in
+        developing complex solutions, interested in writing new code. Has the
+        ability to create effective algorithms, interact positively, and
+        communicate appropriately with team members. Able to grasp new
+        technologies and concepts quickly to develop innovative and creative
+        solutions to problems. Always eager to learn about various technologies,
+        tools, and libraries.
+      </p>
+      <br />
+      <p className="info"><span>Contact:</span>  resulavsar003@gmail.com</p>
+    </div>
+  );
+};
+
+export default About;
